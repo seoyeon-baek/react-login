ac1d4f5734de6b0a454a4b675c9f4c4d1f80bffa
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-07-06 16:10:05 +0900

    Add: login, profile page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Login from "./components/Login/Login";
+import Profile from "./components/Profile/Profile";
+import {Route, BrowserRouter, Routes} from "react-router-dom";
 
 function App() {
+  const token = localStorage.getItem("accessToken");
+  if (!token) {
+    return <Login />;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
